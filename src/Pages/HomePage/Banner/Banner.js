9a7721ab45b6102794418172dee1e1d5db4d14e2
unknown <rajebul.alam17@gmail.com>
2022-05-02 16:37:21 +0600

    Project Initialized

--- src/Pages/HomePage/Banner/Banner.js
+++ src/Pages/HomePage/Banner/Banner.js
@@ -1,9 +1,62 @@
 import React from 'react';
+import './Banner.css';
+import { Carousel } from 'react-bootstrap';
 
 const Banner = () => {
     return (
-        <div>
-            <h2>Banner with Slider</h2>
+        <div className='background'>
+            <div className='size'>
+                <Carousel>
+                    <Carousel.Item>
+                        <div className='slider'>
+                            <div className='w-100'>
+                                <h2>Exception for Everyone with Unique Style</h2>
+                                <p>Dress Sense makes man perfect</p>
+                                <button className='btn text-white'>Buy Now</button>
+                            </div>
+                            <div className='w-100'>
+                                <img
+                                    className="d-block w-100"
+                                    src="https://i.ibb.co/mq9YsxV/men-with-suit-removebg-preview.png"
+                                    alt="First slide"
+                                />
+                            </div>
+                        </div>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <div className='slider'>
+                            <div className='w-100'>
+                                <h2>Experience The Brilliance of a Real Gem</h2>
+                                <p>Fashion is a man soul</p>
+                                <button className='btn text-white'>Buy Now</button>
+                            </div>
+                            <div className='w-100'>
+                                <img
+                                    className="d-block w-100"
+                                    src="https://i.ibb.co/cT2f14S/1-3-removebg-preview.png"
+                                    alt="Second slide"
+                                />
+                            </div>
+                        </div>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <div className='slider'>
+                            <div className='w-100'>
+                                <h2>Choice is your's!</h2>
+                                <p>Product is Our's</p>
+                                <button className='btn text-white'>Order Now</button>
+                            </div>
+                            <div className='w-100'>
+                                <img
+                                    className="d-block w-100 third"
+                                    src="https://i.ibb.co/QrvC8MV/pexels-dom-removebg-preview.png"
+                                    alt="Second slide"
+                                />
+                            </div>
+                        </div>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -2,24 +2,23 @@ import React from 'react';
 import './Header.css';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <Navbar className='py-3' bg="dark" variant="dark" expand="lg">
-                <Container>
-                    <Navbar.Brand className='fw-bold fs-3' href="#home"> <span className='fs-1 text-warning'>D</span>ress <span className='fs-1 text-warning'>W</span>arehouse</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="ms-auto menu-item">
-                            <Nav.Link className='text-white mx-2' href="#home">Home</Nav.Link>
-                            <Nav.Link className='register text-dark px-3 btn mx-2' href="#link">Register</Nav.Link>
-                            <Nav.Link className='login text-dark ms-2 px-4 btn' href="#link">Login</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </div>
+        <Navbar className='py-3' bg="dark" variant="dark" expand="lg">
+            <Container>
+                <Navbar.Brand className='fw-bold fs-3' as={Link} to='/'> <span className='fs-1 text-warning'>D</span>ress <span className='fs-1 text-warning'>W</span>arehouse</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto menu-item">
+                        <Nav.Link className='text-white mx-2' as={Link} to='/home'>Home</Nav.Link>
+                        <Nav.Link className='register text-dark px-3 btn mx-2' href="#link">Register</Nav.Link>
+                        <Nav.Link className='login text-dark ms-2 px-4 btn' href="#link">Login</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
