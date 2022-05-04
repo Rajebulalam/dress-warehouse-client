1fdd3ea6fe68b54ad8029a1acfd0f12bb4c84874
unknown <rajebul.alam17@gmail.com>
2022-05-04 19:50:22 +0600

    Banner Part Responsived

--- src/Pages/HomePage/Banner/Banner.js
+++ src/Pages/HomePage/Banner/Banner.js
@@ -9,12 +9,14 @@ const Banner = () => {
                 <Carousel>
                     <Carousel.Item>
                         <div className='slider'>
-                            <div className='w-100'>
-                                <h2>Exception for Everyone with Unique Style</h2>
-                                <p>Dress Sense makes man perfect</p>
-                                <button className='btn text-white'>Buy Now</button>
+                            <div className='w-100 child-left'>
+                                <div>
+                                    <h2>Exception for Everyone with Unique Style</h2>
+                                    <p>Dress Sense makes man perfect</p>
+                                    <button className='btn text-white'>Buy Now</button>
+                                </div>
                             </div>
-                            <div className='w-100'>
+                            <div className='w-100 child-right'>
                                 <img
                                     className="d-block w-100"
                                     src="https://i.ibb.co/mq9YsxV/men-with-suit-removebg-preview.png"
@@ -25,12 +27,14 @@ const Banner = () => {
                     </Carousel.Item>
                     <Carousel.Item>
                         <div className='slider'>
-                            <div className='w-100'>
-                                <h2>Experience The Brilliance of a Real Gem</h2>
-                                <p>Fashion is a man soul</p>
-                                <button className='btn text-white'>Buy Now</button>
+                            <div className='w-100 child-left'>
+                                <div>
+                                    <h2>Experience The Brilliance of a Real Gem</h2>
+                                    <p>Fashion is a man soul</p>
+                                    <button className='btn text-white'>Buy Now</button>
+                                </div>
                             </div>
-                            <div className='w-100'>
+                            <div className='w-100 child-right'>
                                 <img
                                     className="d-block w-100"
                                     src="https://i.ibb.co/cT2f14S/1-3-removebg-preview.png"
@@ -41,12 +45,14 @@ const Banner = () => {
                     </Carousel.Item>
                     <Carousel.Item>
                         <div className='slider'>
-                            <div className='w-100'>
-                                <h2>Choice is your's!</h2>
-                                <p>Product is Our's</p>
-                                <button className='btn text-white'>Order Now</button>
+                            <div className='w-100 child-left'>
+                                <div>
+                                    <h2>Choice is your's!</h2>
+                                    <p>Product is Our's</p>
+                                    <button className='btn text-white'>Order Now</button>
+                                </div>
                             </div>
-                            <div className='w-100'>
+                            <div className='w-100 child-right'>
                                 <img
                                     className="d-block w-100 third"
                                     src="https://i.ibb.co/QrvC8MV/pexels-dom-removebg-preview.png"
