6478aee6f18dd0ab4f68cd13920d7f9997fb4162
unknown <rajebul.alam17@gmail.com>
2022-05-08 16:44:15 +0600

    Delivered Buuton Clicking Reduce Function Commited

--- src/Pages/InventoryDetails/InventoryDetails.js
+++ src/Pages/InventoryDetails/InventoryDetails.js
@@ -7,16 +7,37 @@ const InventoryDetails = () => {
 
     const { id } = useParams();
     const [user, setUser] = useState({});
+    const [productQuantity, setProductQuantity] = useState(0);
 
     // Load from Server by Id
     useEffect(() => {
         const url = `http://localhost:5000/product/${id}`
         fetch(url)
             .then(res => res.json())
-            .then(data => setUser(data))
+            .then(data => {
+                setUser(data);
+                setProductQuantity(data.quantity);
+            })
     }, [id]);
 
-    // Reducing Function
+    // Update Quantity
+    const handleDeliveredBtn = async () => {
+        const quantity = productQuantity - 1;
+        const newQuantity = { quantity };
+        const url = `http://localhost:5000/product/${id}`;
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newQuantity)
+        })
+            .then(res => res.json())
+            .then(data => {
+                alert('update');
+                setProductQuantity(data?.quantity || quantity);
+            })
+    }
 
 
     return (
@@ -32,11 +53,13 @@ const InventoryDetails = () => {
                         <h3> {user.name} </h3>
                         <p> {user.description} </p>
                         <h5 className='p-0 m-0'> Price : {user.price} </h5>
-                        <h6 className='py-1 m-0'> Quantity : <small>{user.quantity}</small> </h6>
+                        <h6 className='py-1 m-0'> Quantity : <small>{productQuantity}</small> </h6>
                         <h6 className='p-0 m-0'> Supplier : {user.supplier} </h6>
                     </div>
                     <div>
-                        <button className='w-100' type='btn'>Delivered</button>
+                        <button onClick={handleDeliveredBtn} className='w-100' type='btn'>Delivered</button>
+                        <input className='my-2' type="number" name="number" id="number" />
+                        <button className='w-100' type="btn">Restock</button>
                     </div>
                 </div>
             </Container >
