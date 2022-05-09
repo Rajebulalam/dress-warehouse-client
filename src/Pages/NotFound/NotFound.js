6b4b89f408a714dd3c55c59dd7ee9f427453f7f1
unknown <rajebul.alam17@gmail.com>
2022-05-09 15:25:08 +0600

    Not Found Page Responsived

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -8,7 +8,7 @@ const NotFound = () => {
         <div>
             <Container className='py-5 notFound text-center'>
                 <h1>Opps!</h1>
-                <h2 className='text-danger fs-1 fw-bold'>404</h2>
+                <h2 className='text-danger fs-1 fw-bold py-1'>404</h2>
                 <h2 className='p-0'>PAGE NOT FOUND</h2>
                 <p className='fw-bold'>The page you are looking for might have been removed</p>
                 <Link className='px-4 py-2 mt-2 fw-bold rounded' to='/'>Take Me Home</Link>
